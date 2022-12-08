f49a19664f878f3e82025252c161ad5fee859b82
Sugam Shakya <shakyasugam500@gmail.com>
2022-12-08 10:30:34 +0545

    set counter by fucntion call

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,16 +1,27 @@
-import React, {useState} from "react";
-import {AiOutlinePlusCircle, AiOutlineMinusCircle} from 'react-icons/ai' 
-
+import React, { useState } from "react";
+import { AiOutlinePlusCircle, AiOutlineMinusCircle } from "react-icons/ai";
 
 const Counter = () => {
-    const [count, setCount] = useState(0)
-    return(
-        <div>
-            <AiOutlinePlusCircle size={30} color ={'green'} onClick = {() => setCount(count + 1)}/>
-            <h1>{count}</h1> 
-            <AiOutlineMinusCircle size={30} color ={'red'} onClick = {() => setCount(count > 0 ? count - 1 : count)}/>
-        </div>
-    )
-}
+  const [count, setCount] = useState(0);
+  const handlePlus = () => {setCount(count + 1)}
+  const handleMinus = () => {setCount(count > 0 ? count - 1 : count)} 
+  return (
+    <div>
+      <AiOutlinePlusCircle
+        size={30}
+        color={"green"}
+        //onClick={() => setCount(count + 1)}
+        onClick = {handlePlus}
+      />
+      <h1>{count}</h1>
+      <AiOutlineMinusCircle
+        size={30}
+        color={"red"}
+        //onClick={() => setCount(count > 0 ? count - 1 : count)}
+        onClick ={handleMinus}
+      />
+    </div>
+  );
+};
 
-export default Counter
+export default Counter;
